fd95d1aecbe779da0195af9e794f0c47d1cfe4e6
egor <Egor Kiselev>
2024-03-17 12:50:34 +0300

    sqlite repository added

---
+++ bookkeeper/repository/sqlite_repository.py
@@ -0,0 +1,98 @@
+"""
+ This module contains repository working with sqlite3 database 
+"""
+
+import sqlite3
+from itertools import count
+from typing import Any
+from inspect import signature
+
+from bookkeeper.repository.abstract_repository import AbstractRepository, T
+from datetime import datetime
+
+
+class SQLiteRepository(AbstractRepository[T]):
+    """
+    Repository working with sqlite3 database
+    """
+
+    def __init__(self, cls_type: type[T]) -> None:
+        if not hasattr(cls_type, 'pk'):
+            raise ValueError("Trying to create SQLiteRepository over class without 'pk' field")
+        self._cls_type = cls_type
+        self._table_name = cls_type.__name__.lower()
+        self._attributes = list(cls_type.__annotations__.keys())
+        self._attributes.remove('pk')
+        self._connection = sqlite3.connect('bookkeeper.db')
+        self._cursor = self._connection.cursor()
+
+        annot_type_to_sql = {
+            int: 'INTEGER',
+            int | None: 'INTEGER',
+            str: 'TEXT',
+            str | None: 'TEXT',
+            datetime: 'DATETIME',
+            datetime | None: 'DATETIME'
+        }
+        fields = ', '.join([name + ' ' + annot_type_to_sql[tp] for name, tp in cls_type.__annotations__.items() if name != 'pk'])
+        print(f'CREATE TABLE IF NOT EXISTS {self._table_name} (pk INTEGER PRIMARY KEY, {fields})')
+
+        self._cursor.execute(f'CREATE TABLE IF NOT EXISTS {self._table_name} (pk INTEGER PRIMARY KEY, {fields})')
+        self._connection.commit()
+
+    def __del__(self) -> None:
+        self._connection.commit()
+        self._connection.close()
+
+    def tuple_to_object(self, values: list) -> T:
+        obj = self._cls_type()
+        obj.pk = values[0]
+        for attr, val in zip(self._attributes, values[1:]):
+            if self._cls_type.__annotations__[attr] in (datetime, datetime | None) and val is not None:
+                val = datetime.strptime(val, '%Y-%m-%d %H:%M:%S.%f')
+            setattr(obj, attr, val)
+        return obj
+
+
+    def add(self, obj: T) -> int:
+        if (type(obj) != self._cls_type):
+            raise ValueError(f'Trying to add object of wrong type')
+        if getattr(obj, 'pk', None) != 0:
+            raise ValueError(f'Trying to add object {obj} with filled `pk` attribute')
+
+        fields = ', '.join(self._attributes)
+        values = [getattr(obj, attr) for attr in self._attributes]
+        val_flags = '?, ' * (len(values) - 1) + '?'
+        self._cursor.execute(f'INSERT INTO {self._table_name} ({fields}) VALUES ({val_flags})', values)
+        self._cursor.execute('SELECT last_insert_rowid()')
+        self._connection.commit()
+        pk = self._cursor.fetchone()[0]
+        obj.pk = pk
+
+        return pk
+
+    def get(self, pk: int) -> T | None:
+        self._cursor.execute(f'SELECT * FROM {self._table_name} WHERE pk == ?', (pk,))
+        return self.tuple_to_object(self._cursor.fetchone())
+
+    def get_all(self, where: dict[str, Any] | None = None) -> list[T]:
+        if where is None:
+            self._cursor.execute(f'SELECT * FROM {self._table_name}')
+        else:
+            conditions = ', '.join([field + ' == ?' for field in where])
+            values = list(where.values())
+            self._cursor.execute(f'SELECT * FROM {self._table_name} WHERE {conditions}', values)
+
+        return [self.tuple_to_object(t) for t in self._cursor.fetchall()]
+        
+    def update(self, obj: T) -> None:
+        if obj.pk == 0:
+            raise ValueError('attempt to update object with unknown primary key')
+
+        fields = ', '.join([attr + ' = ?' for attr in self._attributes])
+        values = [getattr(obj, attr) for attr in self._attributes]
+        values.append(obj.pk)
+        self._cursor.execute(f'UPDATE {self._table_name} SET {fields} WHERE pk = ?', values)
+
+    def delete(self, pk: int) -> None:
+        self._cursor.execute(f'DELETE FROM {self._table_name} WHERE pk = ?', [pk])
--- bookkeeper/simple_client.py
+++ bookkeeper/simple_client.py
@@ -4,23 +4,39 @@
 
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
-from bookkeeper.repository.memory_repository import MemoryRepository
+# from bookkeeper.repository.memory_repository import MemoryRepository
+from bookkeeper.repository.sqlite_repository import SQLiteRepository
 from bookkeeper.utils import read_tree
 
-cat_repo = MemoryRepository[Category]()
-exp_repo = MemoryRepository[Expense]()
+cat_repo = SQLiteRepository[Category](Category)
+exp_repo = SQLiteRepository[Expense](Expense)
+
+# cat_repo = SQLiteRepository[Category]('category',
+#     ('name', 'parent'),
+#     ('TEXT NOT NULL', 'INTEGER'),
+#     ('FOREIGN KEY(parent) REFERENCES category(pk)'))
+
+# exp_repo = SQLiteRepository[Expense]('expense',
+#     ('amount', 'category', 'expense_date', 'added_date', 'comment'),
+#     ('INTEGER NOT NULL', 'INTEGER', 'DATETIME', 'DATETIME', 'TEXT'),
+#     ('FOREIGN KEY(category) REFERENCES category(pk)'))
 
 cats = '''
-продукты
-    мясо
-        сырое мясо
-        мясные продукты
-    сладости
-книги
-одежда
+products
+    meat
+        raw meat
+        meat products
+    sweets
+books
+clothes
 '''.splitlines()
-
+from inspect import get_annotations
 Category.create_from_tree(read_tree(cats), cat_repo)
+# print(dir(Category('n')))
+# clas = Category('n')
+print(get_annotations(Category, eval_str=True))
+print(Category('n').__annotations__)
+print(Category('n').__dir__)
 
 while True:
     try:
@@ -29,17 +45,19 @@ while True:
         break
     if not cmd:
         continue
-    if cmd == 'категории':
+    if cmd == 'categories':
         print(*cat_repo.get_all(), sep='\n')
-    elif cmd == 'расходы':
+    elif cmd == 'expenses':
         print(*exp_repo.get_all(), sep='\n')
     elif cmd[0].isdecimal():
         amount, name = cmd.split(maxsplit=1)
         try:
             cat = cat_repo.get_all({'name': name})[0]
         except IndexError:
-            print(f'категория {name} не найдена')
+            print(f"category '{name}' not found")
             continue
         exp = Expense(int(amount), cat.pk)
         exp_repo.add(exp)
         print(exp)
+    else:
+        print(f"Unknown command '{cmd}'")
