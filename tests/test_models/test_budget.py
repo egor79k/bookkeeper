3bb2ceb08405d6b7fa5ab86436f5458c9933d467
egor <Egor Kiselev>
2024-04-06 14:23:18 +0300

    Budget model test added

---
+++ tests/test_models/test_budget.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+import pytest
+
+from bookkeeper.repository.memory_repository import MemoryRepository
+from bookkeeper.models.budget import Budget
+
+
+@pytest.fixture
+def repo():
+    return MemoryRepository()
+
+
+def test_create_with_full_args_list():
+    b = Budget(amount=100, limit=1000, period='week', pk=1)
+    assert 100 == b.amount
+    assert 1000 == b.limit
+    assert 'week' == b.period
+    assert 1 == b.pk
+
+
+def test_create_brief():
+    b = Budget(100, 1000)
+    assert 100 == b.amount
+    assert 1000 == b.limit
+
+
+def test_can_add_to_repo(repo):
+    b = Budget(100, 1000)
+    pk = repo.add(b)
+    assert b.pk == pk
