0bcb4de9ac1cc056d4e04bff40a23782801b227a
egor <Egor Kiselev>
2024-04-07 15:10:45 +0300

    Docstrings added in category presenter

--- bookkeeper/presenters/category_presenter.py
+++ bookkeeper/presenters/category_presenter.py
@@ -1,3 +1,5 @@
+""" This module contains presenter class for categories """
+
 from bookkeeper.repository.abstract_repository import AbstractRepository
 
 from bookkeeper.models.expense import Expense
@@ -9,7 +11,15 @@ from bookkeeper.presenters.expense_presenter import ExpensePresenter
 
 
 class CategoryPresenter:
-    def __init__(self, cat_view: AbstractCategoryView, cat_repo: AbstractRepository[Category], exp_repo: AbstractRepository[Expense], exp_presenter: ExpensePresenter) -> None:
+    """
+    Presenter class for categories. Implements logic of CRUD operations.
+    """
+
+    def __init__(self,
+                 cat_view: AbstractCategoryView,
+                 cat_repo: AbstractRepository[Category],
+                 exp_repo: AbstractRepository[Expense],
+                 exp_presenter: ExpensePresenter) -> None:
         self.cat_repo = cat_repo
         self.cat_view = cat_view
         self.exp_repo = exp_repo
@@ -20,12 +30,26 @@ class CategoryPresenter:
 
 
     def add(self, cat: Category) -> None:
+        """
+        Adds new category to repository and view and informs expense presenter.
+        Called from category view.
+
+        Parameters:
+            cat - new Category object with empty pk
+        """
         self.cat_repo.add(cat)
         self.cat_view.add(cat)
         self.exp_presenter.add_category(cat)
 
 
     def update(self, cat: Category) -> None:
+        """
+        Updates an existing category in repository and view and informs expense presenter.
+        Called from category view.
+
+        Parameters:
+            cat - Category object from database
+        """
         self.cat_repo.update(cat)
         self.cat_view.update(cat)
         self.exp_presenter.update_category(cat)
@@ -35,10 +59,18 @@ class CategoryPresenter:
 
 
     def delete(self, pk: int) -> None:
+        """
+        Deletes category by id from repository and view and informs expense presenter.
+        If there are any expenses of this category informs user via category view.
+
+        Parameters:
+            pk - id of Category in database
+        """
         exps_with_cat = self.exp_repo.get_all({'category': pk})
         if len(exps_with_cat) == 0:
             self.cat_repo.delete(pk)
             self.cat_view.delete(pk)
             self.exp_presenter.delete_category(pk)
         else:
-            self.cat_view.warning("Unable to remove category. There are expense(s) of this category.")
+            self.cat_view.warning(
+                "Unable to remove category. There are expense(s) of this category.")
